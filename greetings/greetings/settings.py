66731fdf9c933ae395e49fcab7ec817fc19c8285
xicoarm <43564401+xicoarm@users.noreply.github.com>
2022-12-29 22:19:59 +0100

    first commit

--- greetings/greetings/settings.py
+++ greetings/greetings/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 #TODO
 STRIPE_PUBLISHABLE_KEY = 'pk_test_51L2zmwKwkYYi9AWLs3j1XDMyPbaJxsKiSjNyVqoPbogcH6gVpHZc1FylmuOCnp3GV5wsZT5UriGz77YIRE86WiVH00bIL22Qwg'
 STRIPE_SECRET_KEY = 'sk_test_51L2zmwKwkYYi9AWLSIJsVOufhWiaJ9t3rL9cPufq26GT74DhaYZhzR7KbXIJt1xIUEMhj6ubmMXMSdFhgYvjTBZv00M5KxgZ8n'
-openai.api_key = "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"
+openai.api_key = "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"
 
 # for django >= 3.1
 
